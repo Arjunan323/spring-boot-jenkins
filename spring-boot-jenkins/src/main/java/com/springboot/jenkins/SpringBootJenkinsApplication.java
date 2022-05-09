b440310dac9df60791dd949b4bdcbc101e73cbfa
Arjunan323 <uarjunan97@gmail.com>
2022-05-09 12:39:58 +0530

    first end point

--- spring-boot-jenkins/src/main/java/com/springboot/jenkins/SpringBootJenkinsApplication.java
+++ spring-boot-jenkins/src/main/java/com/springboot/jenkins/SpringBootJenkinsApplication.java
@@ -2,10 +2,21 @@ package com.springboot.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootJenkinsApplication {
 
+	
+	@GetMapping("/message/{name}")
+	public String displayMessage(@PathVariable(name = "name") String name) {
+		return "Hi , " + name + " Welcomr to Jenkins Test";
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 	}
