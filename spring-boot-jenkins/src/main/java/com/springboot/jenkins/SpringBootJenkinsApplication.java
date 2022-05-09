e6df7efb54966e08a1af66016d4d2249eee88181
Arjunan323 <uarjunan97@gmail.com>
2022-05-09 18:39:28 +0530

    tomcat changes

--- spring-boot-jenkins/src/main/java/com/springboot/jenkins/SpringBootJenkinsApplication.java
+++ spring-boot-jenkins/src/main/java/com/springboot/jenkins/SpringBootJenkinsApplication.java
@@ -2,18 +2,19 @@ package com.springboot.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringBootJenkinsApplication {
+public class SpringBootJenkinsApplication extends SpringBootServletInitializer{
 
 	
 	@GetMapping("/message/{name}")
 	public String displayMessage(@PathVariable("name") String name) {
-		return "Hi , " + name + " Welcomr to Jenkins Test";
+		return "Hi , " + name + " Welcome to Jenkins Test";
 		
 	}
 	
